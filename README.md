e3b1a5a63a22468f48711297ba1a195030e5b022
Andrew You <andrew@simplefractal.com>
2020-07-28 12:48:26 -0400

    Privatizes file path variables

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 # Setup
   1. Create virtual environment: `mkvirtualenv -p /usr/local/bin/python3.6 selenium-aws-scraper`
   2. Download requirements.txt.
-  3. Download chromedriver from https://sites.google.com/a/chromium.org/chromedriver/downloads. Download version that matches your Google Chrome version.
+  3. Download chromedriver from https://sites.google.com/a/chromium.org/chromedriver/downloads. Download version that matches your Google Chrome version. Store the path of the chromedriver to an env variable `CHROMEDRIVER_PATH`.
   4. Configure you AWS access key/secret key locally.
   4. Ensure user has role/policy to have lambda/s3 access.
--- base.py
+++ base.py
@@ -1,21 +1,47 @@
 import logging
 import logging.config as loggingConfig
+import os
+
 from selenium import webdriver
 
 class SeleniumBase:
 
     def __init__(self) -> None:
         self.logger = self.initialize_logger()
-        self.webdriver = webdriver.Chrome(executable_path='/Users/andrewyou/Desktop/selenium-aws-webscraper/chromedriver')
+        self.path = os.environ.get('CHROMEDRIVER_PATH')
+        self.webdriver = self.initialize_webdriver()
 
     def __del__(self) -> None:
         self.webdriver.quit()
         self.log('Closed webdriver.')
+        self.log('Closing logging.')
+        logging.shutdown()
+
+    @property
+    def path(self) -> str:
+        self.log('Getting chromedriver path.')
+        return self.__path
+
+    @path.setter
+    def path(self, new_path: str) -> None:
+        self.log(f'Setting chromedriver path to {new_path}.')
+        self.__path = new_path
+
+    @path.deleter
+    def path(self) -> None:
+        self.log('Resetting chromedriver path.')
+        self.__path = ''
+
+    def initialize_webdriver(self) -> webdriver:
+        self.log('Initializing webdriver.')
+        self.webdriver = webdriver.Chrome(executable_path=self.path)
+        self.log('Initialized webdriver.')
+        return self.webdriver
 
     def initialize_logger(self) -> logging.Logger:
         loggingConfig.fileConfig('logging.conf')
         self.logger = logging.getLogger('root')
-        self.log('Instantiated logger.')
+        self.log('Initialized logger.')
         return self.logger
 
     def log(self, msg: str) -> None:
