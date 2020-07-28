f9f5b6f93b2e0a21b6fd89263020e82dbf397901
Andrew You <andrew@simplefractal.com>
2020-07-28 11:59:47 -0400

    Add logging capabilities

--- base.py
+++ base.py
@@ -1,4 +1,22 @@
+import logging
+import logging.config as loggingConfig
+
 class SeleniumBase:
 
     def __init__(self) -> None:
+        self.logger = self.initialize_logger()
+
+    def initialize_logger(self) -> logging.Logger:
+        loggingConfig.fileConfig('logging.conf')
+        self.logger = logging.getLogger('root')
+        self.log('Instantiated logger.')
+        return self.logger
+
+    def log(self, msg: str) -> None:
+        self.logger.info(msg)
+
+    def process(self) -> None:
         pass
+
+if __name__ == '__main__':
+    SeleniumBase().process()
