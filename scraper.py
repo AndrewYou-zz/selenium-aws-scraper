4f29a1ddb3345cf4b8858dc7d6fc7c3311394690
Andrew You <andrew@simplefractal.com>
2020-07-28 16:48:12 -0400

    Navigate to nyc doctors

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
-from base import SeleniumBase
-from selenium_utils import ElementHasCssSelector
-from class_types import WebElements
-
 from selenium.webdriver.support.ui import WebDriverWait
+
+from base import SeleniumBase
+from class_types import WebElements
+from selenium_utils import ElementHasCssSelector
 
 
 class Scraper(SeleniumBase):
@@ -10,22 +10,39 @@ class Scraper(SeleniumBase):
         super().__init__()
         self.base_url = url
 
+    def go_to(self, url: str) -> None:
+        self.log(f"Navigating to {url}.")
+        self.webdriver.get(url)
+        self.log(f"Navigated to {url}.")
+
     def wait_for_elements_by_css_selector(self, css_selector: str) -> WebElements:
+        self.log(f"Waiting for {css_selector!r}.")
         elements = WebDriverWait(self.webdriver, 10).until(
             ElementHasCssSelector(css_selector)
         )
+        self.log(f"Found {len(elements)} {css_selector!r} elements.")
         return elements
 
-    def process(self) -> None:
-        self.log('Beginning scraping process.')
-        self.webdriver.get(self.base_url)
+    def go_to_locations_page(self) -> None:
         elements = self.wait_for_elements_by_css_selector("a[href='/locations']")
         assert len(elements) == 1
-        locations_url = elements[0].get_attribute('href')
-        print(locations_url)
-        self.log('Ending scraping process.')
+        locations_url = elements[0].get_attribute("href")
+        self.go_to(locations_url)
+
+    def go_to_nyc_page(self) -> None:
+        elements = self.wait_for_elements_by_css_selector("a[href='/locations/nyc']")
+        assert len(elements) == 1
+        nyc_url = elements[0].get_attribute("href")
+        self.go_to(nyc_url)
+
+    def process(self) -> None:
+        self.log("Beginning scraping process.")
+        self.go_to(self.base_url)
+        self.navigate_to_locations_page()
+        self.navigate_to_nyc_page()
+        self.log("Ending scraping process.")
 
 
-if __name__ == '__main__':
-    url = 'https://www.onemedical.com/'
+if __name__ == "__main__":
+    url = "https://www.onemedical.com/"
     Scraper(url).process()
